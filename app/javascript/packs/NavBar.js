3495e399a0380fbe40f8f393c64c0790d421320d
tslater3 <trevor.s.slater@gmail.com>
2017-05-09 20:12:31 -0600

    have to move semantic ui into asset pipeline for some reason

--- app/javascript/packs/NavBar.js
+++ app/javascript/packs/NavBar.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { Button, Dropdown, Menu } from 'semantic-ui-react'
+import { Menu } from 'semantic-ui-react'
 
 class NavBar extends Component {
   state = { activeItem: 'home' }
@@ -11,22 +11,12 @@ class NavBar extends Component {
 
     return (
       <Menu size='tiny'>
-        <Menu.Item name='home' active={activeItem === 'home'} onClick={this.handleItemClick} />
-        <Menu.Item name='messages' active={activeItem === 'messages'} onClick={this.handleItemClick} />
-
-        <Menu.Menu position='right'>
-          <Dropdown item text='Language'>
-            <Dropdown.Menu>
-              <Dropdown.Item>English</Dropdown.Item>
-              <Dropdown.Item>Russian</Dropdown.Item>
-              <Dropdown.Item>Spanish</Dropdown.Item>
-            </Dropdown.Menu>
-          </Dropdown>
-
-          <Menu.Item>
-            <Button primary>Sign Up</Button>
-          </Menu.Item>
-        </Menu.Menu>
+        <Menu.Item name='home' active={activeItem === 'home'} onClick={this.handleItemClick}>
+          Home
+        </Menu.Item>
+        <Menu.Item name='messages' active={activeItem === 'messages'} onClick={this.handleItemClick}>
+          Messages
+        </Menu.Item>
       </Menu>
     )
   }
